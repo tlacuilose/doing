3f4c19ded700196d41a6d06108801c57e6b5ccc1
Jose Tlacuilo <tlacuilose@outlook.com>
2022-04-16 00:09:52 -0500

    Encodes and decodes a task.

--- task/task.go
+++ task/task.go
@@ -1,7 +1,14 @@
 // Package defines the task model.
 package task
 
-import "time"
+import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
 
 // Status is the status of the task.
 type Status int64
@@ -12,11 +19,22 @@ const (
 	Completed
 )
 
+// Group all status for reference of length and members.
+var allStatus = []Status{Doing, Completed}
+
 // Task has time when it was created, a description and a status (doing or completed)
 type Task struct {
 	CreationTime string
 	Description  string
 	Status       Status
+}
+
+// Validates that the task has full information
+func (t *Task) validate() error {
+	if t.CreationTime == "" || t.Description == "" || t.Status < 0 {
+		return errors.New("Malformed task.")
+	}
+	return nil
 }
 
 // New creates a new task with the current time and a status of doing.
@@ -26,4 +44,62 @@ func New(description string) Task {
 		Description:  description,
 		Status:       Doing,
 	}
+}
+
+// Encodes a string to bs64 string.
+func b64Encode(text string) string {
+	return base64.StdEncoding.EncodeToString([]byte(text))
+}
+
+// Decodes a string to bs64 string.
+func b64Decode(encodedText string) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(encodedText)
+	return string(data), err
+}
+
+// Encodes task into base64 single line.
+// An encoded task is of string function "base64(CreationTime),base64(Description),Status
+func Encode(task *Task) (string, error) {
+	if err := task.validate(); err != nil {
+		return "", err
+	}
+
+	bTime := b64Encode(task.CreationTime)
+	bDescription := b64Encode(task.Description)
+
+	encodedTask := fmt.Sprintf("%s,%s,%d", bTime, bDescription, task.Status)
+
+	return encodedTask, nil
+}
+
+// Decode task from a string line.
+// An encoded task is of string function "base64(CreationTime),base64(Description),Status
+func Decode(encodedText string) (*Task, error) {
+	fields := strings.Split(encodedText, ",")
+	if len(fields) < 3 {
+		return &Task{}, errors.New("Could not get all field from encoded task.")
+	}
+
+	time, err := b64Decode(fields[0])
+	if err != nil {
+		return &Task{}, err
+	}
+
+	description, err := b64Decode(fields[1])
+	if err != nil {
+		return &Task{}, err
+	}
+
+	statusCode, err := strconv.Atoi(fields[2])
+	if err != nil {
+		return &Task{}, err
+	}
+
+	if statusCode > int(allStatus[len(allStatus)-1]) {
+		return &Task{}, errors.New("Could not get a valid status from encoded task.")
+	}
+
+	status := Status(statusCode)
+
+	return &Task{time, description, status}, nil
 }
